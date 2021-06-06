f9bc4322fedb285e106b8136690cbe5a3c879c5a
PidgeyL <pieter-jan@pidgey.net>
2021-06-06 22:34:33 +0200

    script to generate the alternative tja's

---
+++ bin/gen_tja_from_csv.py
@@ -0,0 +1,71 @@
+import csv
+import os
+
+def update_tja(path, title, sub, song, numbered=False):
+    tja       = read_tja(path)
+    new_tja   = ""
+    orig_dir  = os.path.dirname(path)
+    orig_file = os.path.splitext(path)[0]
+    to_dir    = os.path.join(*os.path.dirname(path).split('/')[:-1], title)
+    to_file   = os.path.join(os.path.dirname(path), title)
+
+    # update TJA
+    for line in tja.splitlines():
+        if line.lower().startswith('title:'):
+            line = line.split(':')[0] + ':' + title
+        if line.lower().startswith('subtitle:'):
+            line = line.split(':')[0] + ':' + sub
+        if line.lower().startswith('wave:'):
+            line = line.split(':')[0] + ':' + title+'.ogg'
+        new_tja = new_tja + line + '\r\n'
+    open(path, 'wb').write(new_tja.encode('utf-8-sig'))
+    # Rename everything
+    if numbered:
+        to_dir = orig_dir.split(' ')[0] + ' ' + to_dir
+    os.rename(orig_file+'.tja', to_file+'.tja')
+    os.rename(orig_file+'.ogg', to_file+'.ogg')
+    os.rename(orig_dir, to_dir)
+
+
+def decode_tja(raw):
+    for enc in ['utf-8-sig', 'utf-8', 'utf-16', 'shift-jis', 'shift-jis_2004']:
+        try:
+            data = raw.decode(enc)
+            assert 'TITLE:' in data
+            return data
+        except Exception as e:
+            pass
+    print("unknown encoding")
+    print("Copy the below in cyberchef for bruteforcing:")
+    print(base64.b64encode(raw[:300]))
+    sys.exit()
+
+
+def read_tja(file):
+    raw = open(file, 'rb').read()
+    return decode_tja(raw)
+
+
+def csv_to_tjas(songfile, to_orig=False, numbered=False, prefix=''):
+    offset = 0 if to_orig else 1
+    with open(songfile, encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for line in list(csv_reader)[1:]:
+            path  = prefix + line[19]
+            title = line[0+offset]
+            sub   = line[2+offset]
+            song  = line[20]
+            update_tja(path, title, sub, song, numbered)
+
+
+if __name__ == '__main__':
+    import argparse
+    argParser = argparse.ArgumentParser(description="Generate tja's in the alternative language")
+    argParser.add_argument('songfile', help='songlist csv')
+    argParser.add_argument('-o', action='store_true',  help='to original: read english, write original')
+    argParser.add_argument('-n', action='store_true',  help='directories have numbers')
+    argParser.add_argument('-p', type=str, default='', help='prefix for the path')
+
+    args = argParser.parse_args()
+
+    csv_to_tjas(args.songfile, to_orig=args.o, numbered=args.n, prefix=args.p)
