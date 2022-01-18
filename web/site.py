dc1a0a5262bf6d5f32b1d2f4c65fb00f6eb9ee6a
PidgeyL <pieter-jan@pidgey.net>
2022-01-18 21:37:36 +0100

    Adding Artist browsing

--- web/site.py
+++ web/site.py
@@ -1,9 +1,11 @@
 import os
+import string
 import sys
 
 run_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(run_path, ".."))
 
+from collections   import defaultdict
 from flask         import render_template, abort, send_from_directory
 from lib.functions import number_format, uniq
 from web.api       import conf, dbl, app
@@ -18,6 +20,20 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/artists', methods=['GET'])
+def artists():
+    data = api.api_artists()
+    adict = defaultdict(list)
+    for artist in data:
+        if artist.name_en[0] in string.ascii_letters:
+            adict[artist.name_en[0].upper()].append(artist.as_dict())
+        else:
+            adict['...'].append(artist.as_dict())
+    for key, val in adict.items():
+        adict[key] = sorted(val, key=lambda x: x['name_en'].lower())
+    return render_template('browse_names.html', names=adict)
+
+
 @app.route('/browse', methods=['GET'])
 def browse():
     data = api.api_browse()
@@ -26,8 +42,9 @@ def browse():
 
 @app.route('/browse_artist/<id>', methods=['GET'])
 def browse_artist(id):
-    data = api.api_browse_artist(id)
-    return render_template('browse.html', songlist=data)
+    data   = api.api_browse_artist(id)
+    artist = dbl.artists.get_by_id(id)
+    return render_template('artist.html', songlist=data, artist=artist)
 
 
 @app.route('/custom/nameplates', methods=['GET'])
